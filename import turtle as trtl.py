6bab5a128c46ed2def13208f58237d809bd851e5
Ryan <ryantuannguyen07@gmail.com>
2025-08-19 10:37:09 -0500

    first commit

---
+++ import turtle as trtl.py
@@ -0,0 +1,28 @@
+import turtle as trtl
+painter = trtl.Turtle()
+
+painter.forward(50)
+painter.left(90)
+painter.forward(50)
+painter.left(90)
+painter.forward(50)
+painter.left(90)
+painter.forward(50)
+painter.right(135)
+painter.forward(50)
+painter.right(45)
+painter.forward(50)
+painter.right(90)
+painter.forward(50)
+painter.right(45)
+painter.forward(50)
+painter.right(135)
+painter.forward(50)
+painter.right(45)
+painter.forward(50)
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
