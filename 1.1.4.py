f53eb6df72ac1fa7a3ecb75caf8b4a699540b5e4
Ryan <ryantuannguyen07@gmail.com>
2025-09-02 10:38:53 -0500

    1.1.4

---
+++ 1.1.4.py
@@ -0,0 +1,20 @@
+#   a114_divisible.py
+
+# get two numbers from user
+num1 = input(" give me 1 numbers")
+num2 = input(" give me 2nd numbers")
+
+# loop while the numbers are not divisible (the remainder is not 0)
+
+while (int(num1) % int(num2) != 0):
+    print("it is non divisible")
+    num1 = input(" give me 1 numbers")
+    num2 = input(" give me 2nd numbers")
+
+print("the numbers are divisible")
+
+  # inform user of result 
+  
+  # gather user input again2
+  
+# inform user of result 
